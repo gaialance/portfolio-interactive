f2b09a17e929d92ba7b4f0093d749bcb7c4793ed
Nicholas Ho Wai Ken <nicholashowaiken@gmail.com>
2023-03-16 00:33:36 +0800

    added some changes to the display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,88 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+'use client'
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image'
+import { useState } from 'react'
+import styles from  './page.module.scss'
 
 export default function Home() {
+
+  const pages = ['Web Page', 'Web Design' , 'Seo Guide','Contact Me']
+  const content = [
+    `I will help you make your
+    design into a web page and
+    advices on how certain features
+    can work together very well `,
+    `You can tell me roughly how you
+    want your design and I will come
+    up with a design that fit your needs.` ,
+    `Figuring out why your content is not
+    reaching the audience that you need?
+    Come explore together and understand.`,
+    `You can contact me via Email at 
+    nicholashowaiken@gmail.com`]
+
+  const [currentIndex, setCurrentIndex] = useState(-1)
+
+  const [showButton, setShowButton] = useState(true)
+
+  const handleClick = (index:number) => {
+    setCurrentIndex(index);
+  }
+
+  const handleButton = () => {
+    setShowButton(!showButton)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+    <div className={styles.container}>
+      <div className={styles.leftContainer}>
+        <Image  src="/oceanPic.png" height={1348} width={1011} alt="ocean" priority/>
+        {currentIndex == -1 && (
+          <>
+            <div className={styles.mainHeader}>
+              <h3>Welcome</h3>
+            </div>
+            <div className={styles.border}></div>
+            <div className={styles.subHeader}>
+              {pages.map((_result,index) =>{
+                return (
+                <>
+                <h2 key={index} onClick={()=>handleClick(index)}>{_result}</h2>
+                </>)
+              })}
+            </div>
+          </>
+        )}
+        {currentIndex > -1 && (
+          <>
+            <div className={styles.mainHeader}>
+              <h3>{pages[currentIndex]}</h3>
+            </div>
+            <div className={styles.border}></div>
+            <div className={styles.subHeader} style={{width:"600px"}}>
+              <h4>
+                {content[currentIndex]}
+              </h4>
+            </div>
+          </>
+        )}
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h1>Nicholas Ho</h1>
         </div>
+        <button className={styles.bottomButton} onClick={()=>handleButton()}>
+            <p className={`${showButton? styles.showAnimation:""}`}>Click me</p>
+        </button>
+        {!showButton && (
+          <h4 className={styles.navigationText} onClick={()=>handleClick(-1)} >Home</h4>
+        )}
+       
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+      <div className={styles.rightContainer}>
+        <Image  src="/oceanPic.png" height={1348} width={1011} alt="ocean" priority/>
+        <h1>
+          Fullstack Developer , Web Design , Seo guideline , Brand Design
+        </h1>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
